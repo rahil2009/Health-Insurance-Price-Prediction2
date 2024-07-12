b8e14411b649456ffa00feb63b584f78e13d6f07
rahil2009 <129578049+rahil2009@users.noreply.github.com>
2024-07-12 22:49:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask,render_template,jsonify,request
 from src.pipeline.prediction_pipeline import CustomData,PredictPipeline
 
-application = Flask(__name__)
-
-app = application
+app = Flask(__name__)
 
 @app.route('/')
 def home_page():
